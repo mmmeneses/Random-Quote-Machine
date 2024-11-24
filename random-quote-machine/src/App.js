3eb4cda2fa97faf234e830f681ed6a1817be9d21
Mauricio Meneses <mmenesesmiller@gmail.com>
2024-11-24 17:28:35 +0000

    quotes added

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -8,16 +8,80 @@ function App() {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
-  /* const quotes = [
+  const quotes = [
+    {
+      text: ' Life is about making an impact, not making an income.',
+      author: 'Kevin Kruse',
+    },
+    
+    {
+      text: 'Whatever the mind of man can conceive and believe, it can achieve.',
+      author: 'Napoleon Hill',
+    },
+
+    {
+      text: 'Strive not to be a success, but rather to be of value.',
+      author: 'Albert Einstein',
+    },
+
     {
       text: '',
       author: '',
-    }
-  ] */
+    },
+
+    {
+      text: '',
+      author: '',
+    },
+
+    {
+      text: '',
+      author: '',
+    },
+
+    {
+      text: '',
+      author: '',
+    },
+
+    {
+      text: '',
+      author: '',
+    },
+
+    {
+      text: '',
+      author: '',
+    },
+
+    {
+      text: '',
+      author: '',
+    },
+
+
+  ]
+  
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  };
+
+  useEffect(() => {
+    const initialQuote = getRandomQuote();
+    setQuote(initialQuote.text);
+    setAuthor(initialQuote.author);
+  }, [getRandomQuote]);
+
+  const handleNewQuote = () => {
+    const newQuote = getRandomQuote();
+    setQuote(newQuote.text);
+    setAuthor(newQuote.author);
+  };
 
   // https://api.quotable.io/random
 
-  const getRandomQuote = async () => {
+  /* const getRandomQuote = async () => {
     try {
       const response = await axios.get('https://api.quotable.io/random');
       return {
@@ -46,7 +110,7 @@ function App() {
 
   const handleNewQuote = () => {
     fetchQuote();
-  }
+  } */
 
   return (
     <div className="App">
