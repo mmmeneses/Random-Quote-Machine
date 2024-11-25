cb68e833f0f565fb905b806a7098c06da72385d1
Mauricio Meneses <mmenesesmiller@gmail.com>
2024-11-25 18:09:40 +0000

    all the quotes added

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -210,55 +210,314 @@ function App() {
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Certain things catch your eye, but pursue only those that capture the heart.',
+      author: 'Ancient Indian Proverb',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Believe you can and you’re halfway there.',
+      author: 'Theodore Roosevelt',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Everything you’ve ever wanted is on the other side of fear.',
+      author: 'George Addair',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.',
+      author: 'Plato',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Teach thy tongue to say, "I do not know," and thous shalt progress.',
+      author: 'Maimonides',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Start where you are. Use what you have.  Do what you can.',
+      author: 'Arthur Ashe',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.',
+      author: 'John Lennon',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Fall seven times and stand up eight.',
+      author: 'Japanese Proverb',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.',
+      author: 'Helen Keller',
     },
 
     {
-      text: '',
-      author: '',
+      text: 'Everything has beauty, but not everyone can see.',
+      author: 'Confucius',
     },
 
+    {
+      text: 'How wonderful it is that nobody need wait a single moment before starting to improve the world.',
+      author: 'Anne Frank',
+    },
+
+    {
+      text: 'When I let go of what I am, I become what I might be.',
+      author: 'Lao Tzu',
+    },
+
+    {
+      text: 'Life is not measured by the number of breaths we take, but by the moments that take our breath away.',
+      author: 'Maya Angelou',
+    },
+
+    {
+      text: 'Happiness is not something readymade.  It comes from your own actions.',
+      author: 'Dalai Lama',
+    },
+
+    {
+      text: 'If you\'re offered a seat on a rocket ship, don\'t ask what seat! Just get on.',
+      author: 'Sheryl Sandberg',
+    },
+
+    {
+      text: 'First, have a definite, clear practical ideal; a goal, an objective. Second, have the necessary means to achieve your ends; wisdom, money, materials, and methods. Third, adjust all your means to that end.',
+      author: 'Aristotle',
+    },
+
+    {
+      text: 'If the wind will not serve, take to the oars.',
+      author: 'Latin Proverb',
+    },
+
+    {
+      text: 'You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground.',
+      author: 'Unknown',
+    },
+
+    {
+      text: 'We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.',
+      author: 'Marie Curie',
+    },
+
+    {
+      text: 'Too many of us are not living our dreams because we are living our fears.',
+      author: 'Les Brown',
+    },
+
+    {
+      text: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.',
+      author: 'Joshua J. Marine',
+    },
+
+    {
+      text: 'If you want to lift yourself up, lift up someone else.',
+      author: 'Booker T. Washington',
+    },
+
+    {
+      text: 'I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.',
+      author: 'Leonardo da Vinci',
+    },
+
+    {
+      text: 'Limitations live only in our minds.  But if we use our imaginations, our possibilities become limitless.',
+      author: 'Jamie Paolinetti',
+    },
+
+    {
+      text: 'You take your life in your own hands, and what happens? A terrible thing, no one to blame.',
+      author: 'Erica Jong',
+    },
+
+    {
+      text: 'What’s money? A man is a success if he gets up in the morning and goes to bed at night and in between does what he wants to do.',
+      author: 'Bob Dylan',
+    },
+
+    {
+      text: 'I didn’t fail the test. I just found 100 ways to do it wrong.',
+      author: 'Benjamin Franklin',
+    },
+
+    {
+      text: 'In order to succeed, your desire for success should be greater than your fear of failure.',
+      author: 'Bill Cosby',
+    },
+
+    {
+      text: 'A person who never made a mistake never tried anything new.',
+      author: 'Albert Einstein',
+    },
+
+    {
+      text: 'The person who says it cannot be done should not interrupt the person who is doing it.',
+      author: 'Chinese Proverb',
+    },
+
+    {
+      text: 'There are no traffic jams along the extra mile.',
+      author: 'Roger Staubach',
+    },
+
+    {
+      text: 'It is never too late to be what you might have been.',
+      author: 'George Eliot',
+    },
+
+    {
+      text: 'You become what you believe.',
+      author: 'Oprah Winfrey',
+    },
+
+    {
+      text: 'I would rather die of passion than of boredom.',
+      author: 'Vincent van Gogh',
+    },
+
+    {
+      text: 'A truly rich man is one whose children run into his arms when his hands are empty.',
+      author: 'Unknown',
+    },
+
+    {
+      text: 'It is not what you do for your children, but what you have taught them to do for themselves, that will make them successful human beings.',
+      author: 'Ann Landers',
+    },
+
+    {
+      text: 'If you want your children to turn out well, spend twice as much time with them, and half as much money.',
+      author: 'Abigail Van Buren',
+    },
+
+    {
+      text: 'Build your own dreams, or someone else will hire you to build theirs.',
+      author: 'Farrah Gray',
+    },
+
+    {
+      text: 'The battles that count aren\'t the ones for gold medals. The struggles within yourself--the invisible battles inside all of us--that\'s where it\'s at.',
+      author: 'Jesse Owens',
+    },
+
+    {
+      text: 'Education costs money.  But then so does ignorance.',
+      author: 'Sir Claus Moser',
+    },
+
+    {
+      text: 'I have learned over the years that when one\'s mind is made up, this diminishes fear.',
+      author: 'Rosa Parks',
+    },
+
+    {
+      text: 'It does not matter how slowly you go as long as you do not stop.',
+      author: 'Confucius',
+    },
+
+    {
+      text: 'If you look at what you have in life, you\'ll always have more. If you look at what you don\'t have in life, you\'ll never have enough.',
+      author: 'Oprah Winfrey',
+    },
+
+    {
+      text: 'Remember that not getting what you want is sometimes a wonderful stroke of luck.',
+      author: 'Dalai Lama',
+    },
+
+    {
+      text: 'You can’t use up creativity.  The more you use, the more you have.',
+      author: 'Maya Angelou',
+    },
+
+    {
+      text: 'Dream big and dare to fail.',
+      author: 'Norman Vaughan',
+    },
+
+    {
+      text: 'Our lives begin to end the day we become silent about things that matter.',
+      author: 'Martin Luther King Jr.',
+    },
+
+    {
+      text: 'Do what you can, where you are, with what you have.',
+      author: 'Teddy Roosevelt',
+    },
+
+    {
+      text: 'If you do what you’ve always done, you’ll get what you’ve always gotten.',
+      author: 'Tony Robbins',
+    },
+
+    {
+      text: 'Dreaming, after all, is a form of planning.',
+      author: 'Gloria Steinem',
+    },
+
+    {
+      text: 'It\'s your place in the world; it\'s your life. Go on and do all you can with it, and make it the life you want to live.',
+      author: 'Mae Jemison',
+    },
+
+    {
+      text: 'You may be disappointed if you fail, but you are doomed if you don\'t try.',
+      author: 'Beverly Sills',
+    },
+
+    {
+      text: 'Remember no one can make you feel inferior without your consent.',
+      author: 'Eleanor Roosevelt',
+    },
+
+    {
+      text: 'Life is what we make it, always has been, always will be.',
+      author: 'Grandma Moses',
+    },
+
+    {
+      text: 'The question isn’t who is going to let me; it’s who is going to stop me.',
+      author: 'Ayn Rand',
+    },
+
+    {
+      text: 'When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.',
+      author: 'Henry Ford',
+    },
+
+    {
+      text: 't’s not the years in your life that count. It’s the life in your years.',
+      author: 'Abraham Lincoln',
+    },
+
+    {
+      text: 'Change your thoughts and you change your world.',
+      author: 'Norman Vincent Peale',
+    },
+
+    {
+      text: 'Either write something worth reading or do something worth writing.',
+      author: 'Benjamin Franklin',
+    },
+
+    {
+      text: 'Nothing is impossible, the word itself says, “I’m possible!”',
+      author: 'Audrey Hepburn',
+    },
+
+    {
+      text: 'The only way to do great work is to love what you do.',
+      author: 'Steve Jobs',
+    },
+
+    {
+      text: 'If you can dream it, you can achieve it.',
+      author: 'Zig Ziglar',
+    }
 
   ]
   
